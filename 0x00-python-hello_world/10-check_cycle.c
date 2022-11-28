deaf9fcf57dc8f3050dc66ee1a1085996dfe88e6
edowaye <edowayeadenaiya@gmail.com>
2022-11-28 11:47:54 -0800

    Wrote a python script that prints a stringto stderr

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+include "lists.h"
+
+/**
+  *check_cycle - function prototype that checks
+  *if a singly linked list has a cycle in it
+  *
+  *@list: the singly linked list to be scanned
+  *
+  *Return: 0 or 1, if there is a cycle or not
+  *respectively
+  *
+  */
+
+int check_cycle(listint_t *list)
+{
+	listint_t
