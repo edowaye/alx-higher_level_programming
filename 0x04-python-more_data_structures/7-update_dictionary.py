b03833345c202ddcb0a966e941feb861c48cb1e7
edowaye <edowayeadenaiya@gmail.com>
2022-12-07 11:49:42 -0800

    Fixed error in code

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for key, value in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, value))
+    a_dictionary.setdefault(key, value)
+    return (a_dictionary)
