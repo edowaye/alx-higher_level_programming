689324045ea14331e69cf5d4819b524dc76eb7f9
edowaye <edowayeadenaiya@gmail.com>
2022-12-07 12:16:50 -0800

    Adjusted code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_score = max(a_dictionary)
-    print("{}: {}".format(best_score)
+    if a_dictionary:
+        my_list = list(a_dictionary.keys())
+	score = 0
+        for i in my_list:
+            if a_dictionary[i] > score:
+               score = a_dictionary[i]
+        return i
